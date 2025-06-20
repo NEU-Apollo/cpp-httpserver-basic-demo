db68a435f1d5250a565d3cf457f8fbb66baf646f
yuan <yuan@qq.com>
2025-06-20 16:00:34 +0800

    示例程序

---
+++ 01httpserver.cpp
@@ -0,0 +1,37 @@
+#include "httplib.h"
+#include <string>
+#include <ctime>
+
+// 创建 HTTP 服务器对象
+httplib::Server svr;
+
+// 示例函数，返回当前时间
+std::string get_current_time() {
+    std::time_t t = std::time(nullptr);
+    std::tm* tm_ptr = std::localtime(&t);
+    char buffer[100];
+    std::strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", tm_ptr);
+    printf("Current time: %s\n", buffer);
+    return std::string(buffer);
+}
+
+int main() {
+    // 设置跨域头，允许所有来源访问
+    svr.set_default_headers({{"Access-Control-Allow-Origin", "*"}}); // 允许所有域名跨域访问
+
+    // 设置 GET 请求的处理方式
+    svr.Get("/hi", [](const httplib::Request &, httplib::Response &res) {
+        res.set_content("Hello World!", "text/plain");
+    });
+
+    // 新的路由处理函数，返回当前时间
+    svr.Get("/get_time", [](const httplib::Request &, httplib::Response &res) {
+        std::string time = get_current_time();
+        res.set_content(time, "text/plain");
+    });
+
+    // 启动服务器
+    svr.listen("0.0.0.0", 8080);
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # cpp-httpserver-basic-demo
 最最基本的cpp实现httpserver服务的demo
+
+## 使用库
+- [cpp-httplib](https://github.com/yhirose/cpp-httplib.git) — 一个 C++11 单文件、头文件库，用于跨平台的 HTTP/HTTPS 请求处理。
+
+
+## demo使用方法
+
+1. 编译运行
+
+    ```cpp
+    g++ -std=c++17 -Wall -Wextra 01httpserver.cpp -o 01httpserver && "/home/weisiyuan/cpp/http_demo/"01httpserver
+    ```
+
+2. 新开一个终端，开一个Python HTTP 服务器
+
+    ```bash
+    python3 -m http.server 8012  # 启动一个简单的 HTTP 服务器，监听 8012 端口
+    ```
+
+3. 访问 http://127.0.0.1:8012/time.html，点击获取时间即可
+
+    http://localhost:8080/hi ：返回 Hello World! 字符串。
+    http://localhost:8080/get_time ：返回当前时间。
+
+
+4. 也可以使用curl测试
+    ```bash
+    curl http://localhost:8080/get_time
+    ```
